a2767f5e882010e96fa5dee7a43f37907534af8a
bmaha2 <maharjanbipin@gmail.com>
2020-04-01 23:59:48 -0700

    time-block background

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 $(document).ready(function () {
     $("#currentDay").append(moment().format("dddd, MMMM Do"));
+    function setHourBlockColor() {
+        $("label").each(function () {
+            //console.log($(this).text());
+            //console.log((moment().format("ha").toUpperCase()));
+            if ($(this).text() > "8AM" && $(this).text() < "6PM") {
+                if ($(this).text() < moment().format("ha").toUpperCase()) {
+                    $(this).next().addClass("past");
+                } else if ($(this).text() === moment().format("ha").toUpperCase()) {
+                    $(this).next().addClass("present");
+                }
+                else {
+                    $(this).next().addClass("future");
+                }
+            } else {
+                $(this).next().addClass("future");
+            }
+        });
+    }
+    //  function addDescription() {
+    //   $(".description").append(".description").text();
+    // }
+    setHourBlockColor();
 });
+
+
+
+
+
+//$(document).ready(function(){
+//    $("#submitBtn").click(function(){        
+//        $("#myForm").submit(); // Submit the form
+//     });
+// });
