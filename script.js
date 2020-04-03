1f7a9f93df01b0f15664c6ef091b9315e586fcce
bmaha2 <maharjanbipin@gmail.com>
2020-04-03 16:17:16 -0700

    final draft

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 $(document).ready(function () {
     $("#currentDay").append(moment().format("dddd, MMMM Do"));
+    renderLastMessage();
+    setHourBlockColor();
+    addDescription();
+
+    function renderLastMessage() {
+        $('input[type="text"]').each(function () {
+            var id = $(this).attr('id');
+            var value = localStorage.getItem(id);
+            $(this).val(value);
+        });
+    }
+
     function setHourBlockColor() {
         $("label").each(function () {
-            //console.log($(this).text());
-            //console.log((moment().format("ha").toUpperCase()));
-            if ($(this).text() > "8AM" && $(this).text() < "6PM") {
-                if ($(this).text() < moment().format("ha").toUpperCase()) {
-                    $(this).next().addClass("past");
-                } else if ($(this).text() === moment().format("ha").toUpperCase()) {
-                    $(this).next().addClass("present");
-                }
-                else {
-                    $(this).next().addClass("future");
-                }
-            } else {
+            // console.log($(this).attr("value"));
+            //console.log((moment().format("k").toUpperCase()));
+            if (moment().format("k") < 08 && moment().format("k") > 17) {
                 $(this).next().addClass("future");
+            }
+            if ($(this).attr("value") < moment().format("k")) {
+                $(this).next().addClass("past");
+            } else if ($(this).attr("value") > moment().format("k")) {
+                $(this).next().addClass("future");
+            }
+            else {
+                $(this).next().addClass("present");
             }
         });
     }
-    //  function addDescription() {
-    //   $(".description").append(".description").text();
-    // }
-    setHourBlockColor();
+    function addDescription() {
+        $(".saveBtn").click(function (e) {
+            e.preventDefault();
+            $('input[type="text"]').each(function () {
+
+                var id = $(this).attr("id");
+                var value = $(this).val();
+                localStorage.setItem(id, value);
+            });
+            renderLastMessage();
+        })
+    }
+
 });
 
-
-
-
-
-//$(document).ready(function(){
-//    $("#submitBtn").click(function(){        
-//        $("#myForm").submit(); // Submit the form
-//     });
-// });
